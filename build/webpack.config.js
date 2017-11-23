d35f1b5a9ec66d0d3b25210cfe12ed25f0d3cf3c
heronghau <heronghau@dianmi365.com>
2017-11-23 13:56:06 +0800

    Update

---
+++ build/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const webpack = require('webpack')
+const Clean = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: {
+        dll: [
+            'vue',
+            'vuex',
+            'vue-router',
+            'zepto-webpack'
+        ]
+    },
+    output: {
+        path: path.resolve(process.cwd(), `dist/`),
+        filename: '[name].js',
+        library: '[name]'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader'
+        }]
+    },
+    plugins: [
+        new webpack.DllPlugin({
+            name: '[name]',
+            path: `dist/[name].json`
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new Clean(['dist'], {
+            root: process.cwd()
+        })
+    ]
+}
